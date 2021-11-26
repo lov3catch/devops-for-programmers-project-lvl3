3cad1de7b33910f45cbc3138d9889764720c8234
Ihor K <lov3catch@gmail.com>
2021-11-26 15:12:51 +0200

    install database

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 resource "digitalocean_droplet" "web" {
-  count = 2
-  image  = "ubuntu-18-04-x64"
-  name   = "web-${count.index}"
-  region = "ams3"
-  size   = "s-1vcpu-1gb"
+  count    = 2
+  image    = "ubuntu-18-04-x64"
+  name     = "web-${count.index}"
+  region   = "ams3"
+  size     = "s-1vcpu-1gb"
   ssh_keys = [data.digitalocean_ssh_key.my_ssh.id]
 }
 
@@ -21,12 +21,26 @@ resource "digitalocean_loadbalancer" "balancer" {
   name   = "balancer"
   region = "ams3"
   forwarding_rule {
-    entry_port      = 80
-    entry_protocol  = "http"
-    target_port     = 80
-    target_protocol = "http"
+    entry_port       = 80
+    entry_protocol   = "http"
+    target_port      = 80
+    target_protocol  = "http"
     certificate_name = digitalocean_certificate.cert.name
   }
-  
+
   droplet_ids = digitalocean_droplet.web.*.id
+}
+
+resource "digitalocean_database_cluster" "db-cluster" {
+  engine     = "pg"
+  version    = "12"
+  name       = "botonarioum-db-cluster"
+  node_count = 1
+  region     = "ams3"
+  size       = "db-s-1vcpu-1gb"
+}
+
+resource "digitalocean_database_db" "db-redmine" {
+  cluster_id = digitalocean_database_cluster.db-cluster.id
+  name       = "redmine"
 }
